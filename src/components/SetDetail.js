9611420f644c8cefbb722620c979ba7e383b287d
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-06-08 10:12:13 +0100

    styling to SetDetail

--- src/components/SetDetail.js
+++ src/components/SetDetail.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const SetDetail = (props) => {
+const SetDetail = ({ set }) => {
+  const { title, artist, thumbnail_image, image } = set;
+  const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.set.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 };
 
 export default SetDetail;
