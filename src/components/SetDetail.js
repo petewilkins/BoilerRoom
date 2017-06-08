eddd0f64fb069af5aef3be9988b22c0c20454b78
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-06-08 09:52:45 +0100

    added CardSection component

--- src/components/SetDetail.js
+++ src/components/SetDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const SetDetail = (props) => {
   return (
     <Card>
-      <Text>{props.set.title}</Text>
+      <CardSection>
+        <Text>{props.set.title}</Text>
+      </CardSection>
     </Card>
   );
 };
