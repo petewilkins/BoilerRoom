9612c1068bfc1a12f0551df8638840112c786343
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-06-08 09:21:56 +0100

    adds axios for API calls

--- src/components/SetList.js
+++ src/components/SetList.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class SetList extends Component {
+  state = { sets: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ sets: response.data }));
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Set List!</Text>
